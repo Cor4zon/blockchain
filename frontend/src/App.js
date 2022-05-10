6dc725b9f64804bc983092a29e9071ac653877a3
Victor Fedorov <vpfedorov@bk.ru>
2022-05-10 19:00:37 +0300

    initial frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Outlet } from "react-router-dom";
+
+import MainContent from "./components/UI/MainContent/MainContent";
+import VotingForm from "./components/UI/VotingForm/VotingForm";
 
 const App = () => {
     return (
         <div>
            <h1>Blockchain voting</h1>
+            <Outlet />
+
         </div>
     );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+import VotingForm from './components/UI/VotingForm/VotingForm';
+import VoterForm from "./components/UI/VoterForm/VoterForm";
+import VotingOptionForm from "./components/UI/VotingOptionForm/VotingOptionForm";
+
+import MainContent from "./components/UI/MainContent/MainContent";
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}>
+                <Route index path="/" element={<MainContent />} />
+                <Route path="voting" element={<VotingForm />} />
+                <Route path="voter" element={<VoterForm />} />
+                <Route path="voting_option" element={<VotingOptionForm />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
 );
