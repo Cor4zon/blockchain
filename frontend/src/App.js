ead5f40db35a4b4794fba9b3acaa96dd9057d1e7
Victor Fedorov <vpfedorov@bk.ru>
2022-05-10 21:02:18 +0300

    link to home page was added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
-import MainContent from "./components/UI/MainContent/MainContent";
-import VotingForm from "./components/UI/VotingForm/VotingForm";
 
 const App = () => {
     return (
         <div>
-           <h1>Blockchain voting</h1>
-            <Outlet />
 
+            <Link to="/">
+                <h1>Blockchain voting</h1>
+            </Link>
+
+            <Outlet />
         </div>
     );
 };
