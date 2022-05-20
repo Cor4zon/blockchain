8388ebab87b86c4cfa992c556bb35a319aa00c85
Victor Fedorov <vpfedorov@bk.ru>
2022-05-20 13:36:26 +0300

    update

---
+++ voter-front/src/components/CreateKeys.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateKeys = () => {
+    return (
+        <div>
+            <p>Todo: create keys</p>
+        </div>
+    );
+};
+
+export default CreateKeys;
